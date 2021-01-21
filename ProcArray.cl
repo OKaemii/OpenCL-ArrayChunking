af668ef8049201562c545f5cba313079d2708daf
Okamii <2314120h@student.gla.ac.uk>
2021-01-21 12:26:26 +0000

    quick-check

--- ProcArray.cl
+++ ProcArray.cl
@@ -1,3 +1,43 @@
+__kernel bool doesIntersect(int aMinX, int aMinY, int aMinZ, int aMaxX, int aMaxY, int aMaxZ, // Block
+	int bMinX, int bMinY, int bMinZ, int bMaxX, int bMaxY, int bMaxZ) // Test Boundary
+{
+	return (aMinX <= bMaxX && aMaxX >= bMinX) &&
+		(aMinY <= bMaxY && aMaxY >= bMinY) &&
+		(aMinZ <= bMaxZ && aMaxZ >= bMinZ);
+}
+
+// super kernel
+/*
+* takes in boundaries of selected chunk, and points to the correct calculation
+*/
+__kernel void doofus(__global int* data, __global int* outData, int x0, int y0, int z0, int w0, int h0, int d0)
+{
+	const unsigned int _WIDTH = 30;
+	const unsigned int _HEIGHT = 30;
+	const unsigned int _DEPTH = 30;
+
+	int x = 0;
+	int z = 0;
+
+	// top boundary?
+	if (doesIntersect(x, _HEIGHT, z, _WIDTH, _HEIGHT * 2 / 3, _DEPTH, x0, y0, z0, w0, h0, d0))
+	{
+		calcTop(data, outData);
+	}
+
+	// mid boundary?
+	if (doesIntersect(x, _HEIGHT * 2 / 3, z, _WIDTH, _HEIGHT * 1 / 3, _DEPTH, x0, y0, z0, w0, h0, d0))
+	{
+		calcMid(data, outData);
+	}
+
+	// bot boundary?
+	if (doesIntersect(x, _HEIGHT * 1 / 3, z, _WIDTH, 0, _DEPTH, x0, y0, z0, w0, h0, d0))
+	{
+		calcBot(data, outData);
+	}
+}
+
 __kernel void calcTop(__global int* data, __global int* outData)
 {
 	outData[get_global_id(0)] = data[get_global_id(0)] * 3;
--- Test.cpp
+++ Test.cpp
@@ -12,46 +12,6 @@ cuint _WIDTH = 30;
 cuint _HEIGHT = 30;
 cuint _DEPTH = 30;
 
-
-// w = width
-// h = height
-// d = depth
-
-bool doesIntersect(int aMinX, int aMinY, int aMinZ, int aMaxX, int aMaxY, int aMaxZ, // Block
-			       int bMinX, int bMinY, int bMinZ, int bMaxX, int bMaxY, int bMaxZ) // Test Boundary
-{
-	return (aMinX <= bMaxX && aMaxX >= bMinX) &&
-		   (aMinY <= bMaxY && aMaxY >= bMinY) &&
-		   (aMinZ <= bMaxZ && aMaxZ >= bMinZ);
-}
-
-bool isTop(int x0, int y0, int z0, int w0, int h0, int d0)
-{
-	cuint x = 0;
-	cuint y = _HEIGHT;
-	cuint z = 0;
-
-	return doesIntersect(x, y,z,_WIDTH, y - _HEIGHT * 1 / 3,_DEPTH,x0,y0,z0,w0,h0,d0);
-}
-
-bool isMid(int x0, int y0, int z0, int w0, int h0, int d0)
-{
-	cuint x = 0;
-	cuint y = _HEIGHT * 2 / 3;
-	cuint z = 0;
-
-	return doesIntersect(x, y, z, _WIDTH, y - _HEIGHT * 1 / 3, _DEPTH, x0, y0, z0, w0, h0, d0);
-}
-
-bool isBot(int x0, int y0, int z0, int w0, int h0, int d0)
-{
-	cuint x = 0;
-	cuint y = _HEIGHT * 1 / 3;
-	cuint z = 0;
-
-	return doesIntersect(x, y, z, _WIDTH, y - _HEIGHT * 1 / 3, _DEPTH, x0, y0, z0, w0, h0, d0);
-}
-
 inline void checkErr(cl_int err, const char* name)
 {
 	if (err != CL_SUCCESS)
@@ -107,21 +67,21 @@ std::vector<int> chunkWork3D_old(cl::Context context, cl::Program program, cl::D
 		cl::Buffer inBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(int) * chunkSize, pVec);
 		cl::Buffer outBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(int) * chunkSize, nullptr);
 
-		if (isTop(x,y,z, x+chunk_x,y+chunk_y,z+chunk_z))
-		{
-			printf("TOP: %d %d %d\n", x, y, z);
-			strcpy_s(calcToPerform, "calcTop");
-		}
-		if (isMid(x, y, z, x+chunk_x, y+chunk_y, z+chunk_z))
-		{
-			printf("MID: %d %d %d\n", x, y, z);
-			strcpy_s(calcToPerform, "calcMid");
-		}
-		if (isBot(x, y, z, x+chunk_x, y+chunk_y, z+chunk_z))
-		{
-			printf("BOT: %d %d %d\n", x, y, z);
-			strcpy_s(calcToPerform, "calcBot");
-		}
+		//if (isTop(x,y,z, x+chunk_x,y+chunk_y,z+chunk_z))
+		//{
+		//	printf("TOP: %d %d %d\n", x, y, z);
+		//	strcpy_s(calcToPerform, "calcTop");
+		//}
+		//if (isMid(x, y, z, x+chunk_x, y+chunk_y, z+chunk_z))
+		//{
+		//	printf("MID: %d %d %d\n", x, y, z);
+		//	strcpy_s(calcToPerform, "calcMid");
+		//}
+		//if (isBot(x, y, z, x+chunk_x, y+chunk_y, z+chunk_z))
+		//{
+		//	printf("BOT: %d %d %d\n", x, y, z);
+		//	strcpy_s(calcToPerform, "calcBot");
+		//}
 
 		// TODO: overlap check
 		// Friday: unit testing
@@ -171,40 +131,12 @@ std::vector<int> chunkWork3D_old(cl::Context context, cl::Program program, cl::D
 	return *vec;
 }
 
-std::vector<int> chunkWork3D(cl::Context context, cl::Program program, cl::Device device, std::vector<int>* vec, cuint halo,
-	cuint chunk_x, cuint chunk_y, cuint chunk_z, int err = 0)
-{
-	int chunkSize = (chunk_x + halo) * (chunk_y + halo) * (chunk_z + halo);
-
-
-	// create buffers, and kernel
-	cl::Buffer inBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(int) * chunkSize, vec);
-	cl::Buffer outBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(int) * chunkSize, nullptr);
-
-	cl::Kernel kernel(program, "calcToPerform", &err);
-	checkErr(err, "kernelling...");
-
-	// fill in args of the user made kernel func in .cl
-	kernel.setArg(0, inBuf);
-	kernel.setArg(1, outBuf);
-
-	cl::CommandQueue queue(context, device);
-
-	// sets up where we to read the finished GPU data
-	err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(chunkSize));
-	checkErr(err, "ND Range kernel execution");
-
-	// reads from GPU data from where it was set to based on NDRangeK
-	err = queue.enqueueReadBuffer(outBuf, CL_FALSE, 0, sizeof(int) * chunkSize, vec); // get data from device, and work on buffer
-	checkErr(err, "Reading buffer...");
-}
-
-std::vector<int> vecToFortran3D(std::vector<int>* vec, cuint halo,cuint x0, cuint x1, cuint y0, cuint y1, cuint z0, cuint z1)
+std::vector<int> vecToFortran3D(std::vector<int>* vec, cuint halo, cuint x0, cuint x1, cuint y0, cuint y1, cuint z0, cuint z1)
 {
 	// take only what we need from our vector
 	// C is row-major order
 	// convert array to column order
-	cuint arr_size = ((x1-x0) + halo) * ((y1-y0) + halo) * ((z1-z0));
+	cuint arr_size = ((x1 - x0) + halo) * ((y1 - y0) + halo) * ((z1 - z0));
 	std::vector<int> dump(arr_size);
 
 	cuint x_begin = x0 - halo > 0 ? x0 - halo : 0;
@@ -256,6 +188,74 @@ std::vector<int> vecToFortran3D(std::vector<int>* vec, cuint halo,cuint x0, cuin
 
 	return dump;
 }
+
+std::vector<int> chunkWork3D(cl::Context context, cl::Program program, cl::Device device, std::vector<int>* vec, cuint halo,
+	cuint chunk_x, cuint chunk_y, cuint chunk_z, int err = 0)
+{
+	int chunkSize = (chunk_x + halo) * (chunk_y + halo) * (chunk_z + halo);
+
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	// initial point to start of our vector
+	for (int* pVec = vec->data(); *pVec < vec->size(); pVec += chunkSize)
+	{
+		// create buffers, and kernel
+		cl::Buffer inBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(int) * chunkSize, pVec);
+		cl::Buffer outBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(int) * chunkSize, nullptr);
+
+		if (x >= _WIDTH)
+		{
+			x = 0;
+			z += chunk_z;
+		}
+
+		if (z >= _DEPTH)
+		{
+			z = 0;
+			y += chunk_y;
+		}
+
+		if (y >= _HEIGHT)
+		{
+			y = 0;
+		}
+
+		x += chunk_x;
+
+		cl::Kernel kernel(program, "doofus", &err);
+		checkErr(err, "kernelling...");
+
+		// fill in args of the user made kernel func in .cl
+		kernel.setArg(0, inBuf);
+		kernel.setArg(1, outBuf);
+
+		kernel.setArg(2, x); // x0
+		kernel.setArg(3, y); // y0
+		kernel.setArg(4, z); // z0
+
+		kernel.setArg(5, x + chunk_x); // w0
+		kernel.setArg(6, y + chunk_y); // h0
+		kernel.setArg(7, z + chunk_z); // d0
+
+		cl::CommandQueue queue(context, device);
+
+		// sets up where we to read the finished GPU data
+		err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(chunkSize));
+		checkErr(err, "ND Range kernel execution");
+
+		// get array to send
+		std::vector<int> dump = vecToFortran3D(vec, halo, x, x + chunk_x, y, y + chunk_y, z, z + chunk_z);
+
+		// reads from GPU data from where it was set to based on NDRangeK
+		err = queue.enqueueReadBuffer(outBuf, CL_FALSE, 0, sizeof(int) * chunkSize, dump.data()); // get data from device, and work on buffer
+		checkErr(err, "Reading buffer...");
+	}
+
+	return *vec;
+}
+
 int main()
 {
 	// create platform to accommodate for devices
@@ -299,7 +299,7 @@ int main()
 	cuint array_height = _HEIGHT;
 	cuint array_depth = _DEPTH;
 	cuint array_size = array_width * array_depth * array_height;
-	// int* array_3d = new int[array_size]; // feeling cute, might delete later
+	// int* array_3d = new int[array_size]; // delete later
 
 	// initialise 3d array
 	// memset(array_3d, 1,array_size);
@@ -315,8 +315,6 @@ int main()
 
 	// perform chunking function
 	vec = chunkWork3D(context, program, device, &vec, array_size, chunkSize_width, chunkSize_height, chunkSize_depth, err);
-
-	// TODO: Unit Tests
 	
 	for (auto &i:vec)
 	{
